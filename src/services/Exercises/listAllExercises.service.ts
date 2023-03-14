d4438fc200be8721458163dab84cb860f8c91161
Luís Nunes <luis.ebn98@gmail.com>
2023-03-14 09:57:01 -0300

    fix: listAllExercises

--- src/services/Exercises/listAllExercises.service.ts
+++ src/services/Exercises/listAllExercises.service.ts
@@ -1,10 +1,13 @@
 import { prisma } from "../../app";
 import { iExerciseList } from "../../interfaces";
+import { exerciseListSchema } from "../../schemas";
 
 export const listAllExercisesService = async (): Promise<iExerciseList> => {
-  const exerciseList = prisma.exercise.findMany({
+  const exerciseList = await prisma.exercise.findMany({
     include: { muscle: true },
   });
 
-  return exerciseList;
+  const validatedList = exerciseListSchema.parse(exerciseList);
+
+  return validatedList;
 };
