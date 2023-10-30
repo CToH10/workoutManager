9ed19031de2c1297d87523268f1e7b765a6cd4cf
CToH10 <luis.ebn98@gmail.com>
2023-10-30 13:38:17 -0300

    feat: add order on workout

--- src/services/DailyWorkout/listWorkoutByUser.service.ts
+++ src/services/DailyWorkout/listWorkoutByUser.service.ts
@@ -6,6 +6,7 @@ export const listWorkoutByUserService = async (id: number) => {
     where: { id: id },
     include: {
       daily_workout: {
+        orderBy: { date: "asc" },
         include: {
           daily_exercise: {
             select: {
